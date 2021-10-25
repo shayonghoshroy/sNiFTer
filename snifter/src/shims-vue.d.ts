79e0b637bfa7161b1b3f1d8554d7ecf8997a94dc
Charles Coffin <cdcoffin@email.sc.edu>
2021-10-24 23:26:22 -0400

    Migrated from mocha to jest test framwork

--- snifter/src/shims-vue.d.ts
+++ snifter/src/shims-vue.d.ts
@@ -1,6 +1,12 @@
 /* eslint-disable */
 declare module '*.vue' {
+  import Vue from 'vue';
+  export default Vue;
+}
+/*
+declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+*/
