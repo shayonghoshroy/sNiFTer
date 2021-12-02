854820dcc28c82f8f5a338ded62e19394fe9d2f4
Shayon Ghoshroy <sghoshroy21@gmail.com>
2021-12-01 23:10:45 -0500

    reinitialized amplify environment

---
+++ snifter/src/graphql/subscriptions.js
@@ -0,0 +1,48 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTweet = /* GraphQL */ `
+  subscription OnCreateTweet {
+    onCreateTweet {
+      id
+      text
+      username
+      reply_count
+      retweet_count
+      like_count
+      interaction_count
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateTweet = /* GraphQL */ `
+  subscription OnUpdateTweet {
+    onUpdateTweet {
+      id
+      text
+      username
+      reply_count
+      retweet_count
+      like_count
+      interaction_count
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteTweet = /* GraphQL */ `
+  subscription OnDeleteTweet {
+    onDeleteTweet {
+      id
+      text
+      username
+      reply_count
+      retweet_count
+      like_count
+      interaction_count
+      createdAt
+      updatedAt
+    }
+  }
+`;
--- snifter/src/main.js
+++ snifter/src/main.js
@@ -2,18 +2,19 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "bulma/css/bulma.css";
-import Amplify from "aws-amplify";
-import aws_exports from "./aws-exports";
 import Oruga from "@oruga-ui/oruga-next";
 import "@oruga-ui/oruga-next/dist/oruga.css";
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
 import {
   applyPolyfills,
   defineCustomElements,
-} from "@aws-amplify/ui-components/loader";
+} from '@aws-amplify/ui-components/loader';
 
 Amplify.configure(aws_exports);
 applyPolyfills().then(() => {
   defineCustomElements(window);
 });
+
 
 createApp(App).use(router).use(Oruga).mount("#app");
