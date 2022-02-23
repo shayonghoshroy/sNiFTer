80bfe1fabde81a0c6f7eb99a1dbda81ac7d4f217
grahammc-bit <grahammc@email.sc.edu>
2022-02-22 19:01:27 -0500

    Pushing arbitrary changes in preperation for later commits

--- snifter/src/components/TweetComponent.vue
+++ snifter/src/components/TweetComponent.vue
@@ -4,8 +4,27 @@
       <div class="post" v-for="tweet in tweets" :key="tweet.id">
         <div class="row center">
           <div class="flex md6 lg4">
+            <Tweet
+              tweet-id="tweet.id"
+              cards="visible"
+              conversation="all"
+              lang="en"
+              theme="light"
+              align="left"
+              :width="400"
+              :dnt="false"
+              @tweet-load-error="onTweetLoadError"
+              @tweet-load-success="onTweetLoadSuccess"
+            >
+              <template v-slot:loading>
+                <span>Loading...</span>
+              </template>
+              <template v-slot:error>
+                <span>Sorry, that tweet doesn’t exist!</span>
+              </template>
+            </Tweet>
             <va-card tag="b">
-              <va-card-title>{{ tweet.username }}</va-card-title>
+              <va-card-title>{{ tweet.id }}</va-card-title>
               <va-card-content>{{ tweet.text }}</va-card-content>
             </va-card>
           </div>
@@ -16,9 +35,25 @@
 </template>
 
 <script>
+import { defineComponent } from "vue";
+import Tweet from "vue-tweet";
 import { API } from "aws-amplify";
 import { listTweets } from "../graphql/queries";
-export default {
+export default defineComponent({
+  components: {
+    Tweet,
+  },
+  setup() {
+    function onTweetLoadSuccess(embedNode) {
+      console.log(embedNode);
+    }
+
+    function onTweetLoadError() {
+      console.log("Ops... an error has occurred");
+    }
+
+    return { onTweetLoadSuccess, onTweetLoadError };
+  },
   name: "TweetComponent",
   async created() {
     this.getTweets();
@@ -27,7 +62,7 @@ export default {
     return {
       username: "",
       text: "",
-      tweets: [],
+      tweets: [].toString(),
     };
   },
   methods: {
@@ -40,15 +75,17 @@ export default {
           },
         });
         this.tweets = tweets.data.listTweets.items;
+        console.log(this.tweets);
       } catch (e) {
         console.error(e);
       }
     },
   },
-};
+});
 </script>
 <style scoped>
 .post {
+  background: #fff;
   padding: 1.5em;
 }
 .post h2 {
