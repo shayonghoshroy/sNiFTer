a925d0b36ba1700f4aa58e83d7c48dc4e612047b
EvanOwre <71037532+EvanOwre@users.noreply.github.com>
2022-02-28 01:00:21 -0500

    Update News.vue

--- snifter/src/views/News.vue
+++ snifter/src/views/News.vue
@@ -1,21 +1,62 @@
 <template>
-  <div>
+  <div class="search">
     <article>
-      <h2 style="color: white">sNiFTer News</h2>
-      <p style="color: white">See what's hot in the world of NFT's</p>
+      <h2>sNiFTer News</h2>
+      <p>See what's hot in the world of NFT's</p>
     </article>
-    <TweetComponent />
+    <TweetSearchComponent ref="tweetSearchComponent" @twitterSearch="searchResults($event)"/>
+    <div v-if="searched">
+      <div v-if="searchStatus === 'Fetching'" class="pending-search-result">
+        <va-inner-loading :loading="true" color="primary">
+          <va-card>
+            <va-card-content>
+              Fetching Tweets...
+            </va-card-content>
+          </va-card>
+        </va-inner-loading>
+      </div>
+      <div class="empty-search-results" v-else-if="searchStatus === 'Failure'">
+        <va-alert class="search-alert mb-4" color="danger" icon="info">
+            No search results...
+        </va-alert>
+      </div>
+      <div v-else>
+        <TweetComponent :tweets="data" />
+      </div>
+    </div>
+    <div v-else>
+      <TweetComponent />
+    </div>
   </div>
 </template>
 
 <script>
 import TweetComponent from "../components/TweetComponent";
+import TweetSearchComponent from '../components/TweetSearchComponent';
 export default {
   name: "News",
-  components: { TweetComponent },
+  components: { TweetComponent, TweetSearchComponent },
+  setup() {
+    return {};
+  },
+  data() {
+    return {
+      data: [],
+      searched: false,
+      searchType: '',
+      searchStatus: 'None'
+    }
+  },
+  methods: {
+    searchResults(event) {
+      this.searchStatus = event.searchStatus;
+      this.searchType = event.searchType;
+      this.data = event.data;
+      this.searched = true;
+    }
+  }
 };
 </script>
-
 <style scoped>
 div {
   display: flex;
@@ -48,5 +89,35 @@ h2 {
   font-size: clamp(24px, 3vw, 3vw);
   font-weight: 300;
   margin: 0;
+}
+blockquote.twitter-tweet {
+  display: inline-block;
+  font-family: "Helvetica Neue", Roboto, "Segoe UI", Calibri, sans-serif;
+  font-size: 12px;
+  font-weight: bold;
+  line-height: 16px;
+  border-color: #eee #ddd #bbb;
+  border-radius: 5px;
+  border-style: solid;
+  border-width: 1px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.15);
+  margin: 10px 5px;
+  padding: 0 16px 16px 16px;
+  max-width: 468px;
+}
+blockquote.twitter-tweet p {
+  font-size: 16px;
+  font-weight: normal;
+  line-height: 20px;
+}
+blockquote.twitter-tweet a {
+  color: inherit;
+  font-weight: normal;
+  text-decoration: none;
+  outline: 0 none;
+}
+blockquote.twitter-tweet a:hover,
+blockquote.twitter-tweet a:focus {
+  text-decoration: underline;
 }
 </style>
