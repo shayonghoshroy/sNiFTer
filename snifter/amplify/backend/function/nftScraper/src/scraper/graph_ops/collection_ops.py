f4ee1500ba714946d44f5f274afd9f8c50903798
grahammc-bit <grahammc@email.sc.edu>
2022-02-27 19:24:16 -0500

    Working wawtchlist component

--- snifter/amplify/backend/function/nftScraper/src/scraper/graph_ops/collection_ops.py
+++
@@ -1,9 +0,0 @@
-from boto3.dynamodb.conditions import Key
-
-# DynamoDB should be passed in from boto3 resource
-def query_collection(slug: str, dynamodb):
-   table = dynamodb.Table('Collection')
-   response = table.query(
-       KeyConditionExpression=Key('slug').eq(slug)
-   )
-   return response.get('items', None)
