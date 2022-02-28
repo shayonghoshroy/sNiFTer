1c747b757e6fc13eaed8d65def692ed4e7cc3e07
EvanOwre <71037532+EvanOwre@users.noreply.github.com>
2022-02-28 01:03:55 -0500

    Update index.py

--- snifter/amplify/backend/function/scrapeTweets/src/index.py
+++ snifter/amplify/backend/function/scrapeTweets/src/index.py
@@ -1,69 +1,173 @@
 import boto3
 from datetime import datetime
-import snscrape.modules.twitter as sntwitter
+import tweepy
+import os
+import json
 
-
-def tw_scraper():
+def scrape_user(api, usernames):
     client = boto3.client('dynamodb')
-
-    #Query by text search
-    maxTweets = 500
-    for i, tweet in enumerate(sntwitter.TwitterSearchScraper('nft').get_items()):
-    #^^ loop for search scraping
-    #for i, tweet in enumerate(sntwitter.TwitterUserScraper('FluffyHeadsNFT').get_items()):
-    #^^ loop for user scraping
-    #for i, tweet in enumerate(sntwitter.TwitterHashtagScraper('nft').get_items()):
-        if i > maxTweets:
-            break
-        '''
-        tweet_list.append([tweet.date, 
-            tweet.id,
-            tweet.content,
-            tweet.user.username,
-            tweet.replyCount, 
-            tweet.retweetCount, 
-            tweet.likeCount,
-            tweet.replyCount + tweet.retweetCount + tweet.likeCount])
-        '''
+    for username in usernames:
+        try:
+            user = api.get_user(id=username)
+        except Exception as e:
+            continue
         data = client.put_item(
-        TableName='tweet-nym7x5baijbtza2crmjtn6bo2q-dev',
+        TableName= 'twitterUser-lwzlz2gonfcnno5mdka25xn2u4-eowredev',
         Item={
             'id': {
-                'S': str(tweet.id)
+                'S': str(user.id)
             },
-            'text': {
-                'S': tweet.content
+            'name': {
+                'S': user.name
             },
-            'username': {
-                'S': tweet.user.username
+            'screen_name': {
+                'S': user.screen_name
             },
-            'reply_count': {
-                'N': str(tweet.replyCount)
+            'location': {
+                'S': user.location
             },
-            'retweet_count': {
-                'N': str(tweet.retweetCount)
+            'description': {
+                'S': user.description
             },
-            'like_count': {
-                'N': str(tweet.likeCount)
+            'URL': {
+                'S': str(user.url)
             },
-            'interaction_count': {
-                'N': str(tweet.replyCount + tweet.retweetCount + tweet.likeCount)
+            'followers': {
+                'N': str(user.followers_count)
             },
-        } 
-    )
-    
-    
+            'friends_count': {
+                'N': str(user.friends_count)
+            },
+            'listed_count': {
+                'N': str(user.listed_count)
+            },
+            'acct_created_at': {
+                'S': str(user.created_at)
+            },
+            'verified': {
+                'BOOL': user.verified
+            }
+        })
+        
+def scrape_user_tweets(api, users, numtweet):
+    client = boto3.client('dynamodb')
+    for user in users:
+        try:
+            tweets = tweepy.Cursor(api.user_timeline, id=user, tweet_mode='extended').items(numtweet)
+        except Exception as e:
+            continue
+        list_tweets = [tweet for tweet in tweets]
+        for tweet in list_tweets:
+            print(str(tweet.id) + " " + tweet.user.screen_name)
+            put_tweet(tweet)
+                    
+def scrape_hashtag(api, words, numtweet):
+        client = boto3.client('dynamodb')
+        for word in words:
+            tweets = tweepy.Cursor(api.search_tweets, word, lang="en", tweet_mode='extended', count = numtweet).items(numtweet)
+            list_tweets = [tweet for tweet in tweets]
+            for tweet in list_tweets:
+                put_tweet(tweet)
+                
+def put_tweet(tweet):
+    client = boto3.client("dynamodb")
+    try:
+            text = tweet.retweeted_status.full_text
+    except AttributeError:
+            text = tweet.full_text
+    data = client.put_item(
+    TableName='tweet-lwzlz2gonfcnno5mdka25xn2u4-eowredev',
+    Item={
+        'id': {
+            'S': str(tweet.id)
+        },
+        'URL': {
+            'S': 'https://twitter.com/twitter/statuses/'+ str(tweet._json['id'])
+        },
+        'text': {
+            'S': text.lower()
+        },
+        'username': {
+            'S': tweet.user.screen_name
+        },
+        'followers_count': {
+            'N': str(tweet.user.followers_count)
+        },
+        'retweet_count': {
+            'N': str(tweet.retweet_count)
+        },
+        'following_count': {
+            'N': str(tweet.user.friends_count)
+        },
+        'date': {
+            'S': str(tweet.created_at)
+        },
+        'favorite_count': {
+            'N': str(tweet.favorite_count)
+        }
+    })
 
+def auth():
+        CONSUMER_KEY = os.environ['TWITTER_CONSUMER_KEY']
+        CONSUMER_SECRET = os.environ['TWITTER_SECRET_KEY']
+        ACCESS_TOKEN = os.environ.get('TWITTER_ACCESS_TOKEN')
+        ACCESS_TOKEN_SECRET = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
+
+
+        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+        api = tweepy.API(auth)
+        return api
+        
 def handler(event, context):
-  tw_scraper()
-
-  response = {
-      'statusCode': 200,
-      'body': 'successfully created item!',
-      'headers': {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-      },
-  }
-  
-  return response
+    #Connnect to twitter API
+    api = auth()
+    
+    #Cloudwatch trigger
+    #scrape_hashtag(api, "nft", 50)
+    #All events should contain event_type field of funciton switch
+    event_type = event.get("resource", None).replace("/", "")
+    
+    #Bad Request
+    if event_type is None or not event_type in ["tweet", "twitteruser"]:
+        return {
+            'statusCode': 400,
+            'headers': {
+                'Access-Control-Allow-Headers': '*',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            },
+            'body': json.dumps({
+                'Exception': 'Invalid Event Type',
+                'Reason': f"{event_type} is not supported"
+            }),
+            'isBase64Encoded': False
+        }
+    
+    
+    #Parse body to a dict
+    obj = json.loads(event["body"])
+    
+    #Parse word out of dict
+    word = obj.get("word")
+    
+    #Parse num of tweets out of obj dict
+    if obj.get("numtweet") is not None:
+        num = obj.get("numtweet")
+    else:
+        num = 10
+    if event_type == "tweet":
+        scrape_hashtag(api, word, num)
+    elif event_type == "twitteruser":
+        scrape_user(api, word)
+        scrape_user_tweets(api, word, num)
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
+        'body': '',
+        'isBase64Encoded': False
+    }
